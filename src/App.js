7a354c98f03c06e116a762d48451df0f17daabb9
Neha-skcet <151619900+Neha-skcet@users.noreply.github.com>
2023-12-07 11:28:06 +0530

    Add files via upload
    question 19

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import UsersComponent from './Exercise19';
+
+function App() {
+  return (
+    <div className="App">
+      <UsersComponent />
+    </div>
+  );
+}
+
+export default App;
